2f86262736c4e7662451587e87730bdf319bfd79
Guillermo Alonso <guialonun@gmail.com>
2026-03-21 11:06:45 +0200

    Rewrite AboutPage

--- src/About.tsx
+++ src/About.tsx
@@ -1,49 +1,23 @@
 export function AboutPage() {
   return <div id="about-page">
     <img className="img-centered" src={`${import.meta.env.BASE_URL}img/chesslab-banner.png`} alt=""></img>
-    <h1>About the author</h1>
-    <p>
-      ¡Qué pasa, gente! My name is Guillermo, aka Guimotron as per
-      my <a href="https://www.youtube.com/@guimotron">
-        <img className="img-inline" src={`${import.meta.env.BASE_URL}svg/youtube.svg`} alt=""></img>
-        Youtube channel
-      </a>.
-      In late 2024, I embarked on a journey to reach 2000 rating on Lichess,
-      recording -almost all of- my games and uploading them to my channel.
-      The journey concluded (successfully!) on early 2026.
-    </p>
 
+    <h1>About the app</h1>
     <p>
-      If you find the tool useful, want to support me, or why not,
-      both, feel free
-      to <a href="https://buymeacoffee.com/guimotron">
-        buy me a coffee
-      </a>!
-      Support is always appreciated but never needed.
-    </p>
-
-    <img className="img-centered" src={`${import.meta.env.BASE_URL}img/guimotron.jpeg`} alt=""></img>
-
-    <h1>About ChessLab</h1>
-    <p>
-      An app built to offer custom functionality that I found lacking
-      in other applications.
+      Welcome to ChessLab, a completely free app offering different
+      chess-related tools that crossed my mind at some point!
 
       Source code can be found in <a href="https://github.com/Flood1993/chesslab">Github</a>.
+
+      Please reach out to me with any feedback or suggestions you may have!
     </p>
 
     <h2>Opening training</h2>
 
     <p>
-      A tool that allows the user to drill opening lines by loading
-      them from a Lichess study.
-      By default it will show the ones I use, so hopefully I learn
-      something about them!
-    </p>
-
-    <p>
-      Note one can also supply a lichess study URL and all the lines
-      from the study will be loaded.
+      Train opening lines by repetition!
+      By default it will load the ones I use, but it is also possible to
+      load them from a Lichess study.
     </p>
 
     <h2>Review game</h2>
@@ -69,5 +43,35 @@ export function AboutPage() {
       Pick a game from the list, choose a color and see how your moves
       compare to those played in the real game!
     </p>
+
+    <h1>About the author</h1>
+    <p>
+      ¡Qué pasa, gente! My name is Guillermo, aka Guimotron as per
+      my <a href="https://www.youtube.com/@guimotron">
+        <img className="img-inline" src={`${import.meta.env.BASE_URL}svg/youtube.svg`} alt=""></img>
+        Youtube channel
+      </a>.
+      In late 2024, I embarked on a journey to reach 2000 rating on Lichess,
+      recording -almost all of- my games and uploading them to my channel.
+      The journey concluded (successfully!) on early 2026.
+    </p>
+
+    <p>
+      Ideas for different tools crossed my mind throughout this journey,
+      so at some point I decided to start implementing them even if only
+      for personal use and the learning experience.
+    </p>
+
+    <p>
+      If you find the tool useful, want to support me, or why not,
+      both, feel free
+      to <a href="https://buymeacoffee.com/guimotron">
+        buy me a coffee
+      </a>!
+      Support is always appreciated but never needed.
+    </p>
+
+    <img className="img-centered" src={`${import.meta.env.BASE_URL}img/guimotron.jpeg`} alt=""></img>
+
   </div>;
 }
