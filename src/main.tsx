90a2c3301c9e5c2bcdcf0ac28e17f0581f268316
Guillermo Alonso <guialonun@gmail.com>
2026-03-22 01:03:08 +0200

    Use merida piece set and blue board

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 import '@lichess-org/chessground/assets/chessground.base.css'
-// Board
-import '@lichess-org/chessground/assets/chessground.brown.css'
-// Pieces
-import '@lichess-org/chessground/assets/chessground.cburnett.css'
+import './chessground.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
